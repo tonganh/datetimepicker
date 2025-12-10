acd2e181baf86806f33f5eb106df05b8e0b63841
anhtn <anh.tndev40@gmail.com>
2025-12-10 18:04:44 +0700

    Feat: fix onChange code

--- ios/RNDateTimePicker.m
+++ ios/RNDateTimePicker.m
@@ -23,13 +23,13 @@
 - (instancetype)initWithFrame:(CGRect)frame
 {
   if ((self = [super initWithFrame:frame])) {
-    #ifndef RCT_NEW_ARCH_ENABLED
+    // #ifndef RCT_NEW_ARCH_ENABLED
       // somehow, with Fabric, the callbacks are executed here as well as in RNDateTimePickerComponentView
       // so do not register it with Fabric, to avoid potential problems
       [self addTarget:self action:@selector(didChange)
                forControlEvents:UIControlEventValueChanged];
       [self addTarget:self action:@selector(onDismiss:) forControlEvents:UIControlEventEditingDidEnd];
-    #endif
+    // #endif
 
     _reactMinuteInterval = 1;
   }
